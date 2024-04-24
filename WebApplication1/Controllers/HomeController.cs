2a3eb273da6b9de30980e4f4cede45e5fa2f3c89
Melanchenko_Andriy <ipz232_mad@student.ztu.edu.ua>
2024-04-24 22:31:00 +0300

    task completed

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string firstName)
+        {
+            ViewBag.firstName = string.Format($"{firstName} is gay");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
